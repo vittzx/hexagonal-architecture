6a05b1b9594dc1d4e385a7630a4835f79b40f186
Vitor Cosso <vitorcosso@gmail.com>
2025-03-09 00:35:58 -0300

    first commit

---
+++ src/main/java/cosso/com/example/hexagonal_architecture/HexagonalArchitectureLearningApplication.java
@@ -0,0 +1,13 @@
+package cosso.com.example.hexagonal_architecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexagonalArchitectureLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexagonalArchitectureLearningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cosso/com/example/hexagonal_architecture/HexagonalArchitectureLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package cosso.com.example.hexagonal_architecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonalArchitectureLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
