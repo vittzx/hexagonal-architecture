c9680999c854a61deb0154537ef4089d8cc85c3f
Vitor Cosso <vitorcosso@gmail.com>
2025-03-21 12:52:20 -0300

    refactor: changing ExceptionResponse time data type

--- src/main/java/cosso/com/example/hexagonal_architecture/infrastructure/adapter/output/handlerexception/data/response/ExceptionResponse.java
+++ src/main/java/cosso/com/example/hexagonal_architecture/infrastructure/adapter/output/handlerexception/data/response/ExceptionResponse.java
@@ -3,19 +3,18 @@ package cosso.com.example.hexagonal_architecture.infrastructure.adapter.output.h
 import cosso.com.example.hexagonal_architecture.infrastructure.adapter.config.MessageConfig;
 import lombok.*;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 
 @Data
 @Builder
-@ToString
 public class ExceptionResponse {
 
-    //private LocalDateTime time;
+    private String time;
     private String status;
     private String message;
 
     public ExceptionResponse(String status, Object... args){
-        // this.time = LocalDateTime.now();
+        this.time = LocalDate.now().toString();
         this.status = status;
         this.message = new MessageConfig().getMessage(status, args);
     }
